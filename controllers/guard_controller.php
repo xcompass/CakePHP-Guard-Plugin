40871607df0cbd4b403377b629b12a6faf828e69
Pan Luo <pan.luo@ubc.ca>
2010-08-25 23:01:58 +0000

    initial import

---
+++ controllers/guard_controller.php
@@ -0,0 +1,30 @@
+<?php
+
+class GuardController extends AppController {
+    var $name = "Guard";
+    var $uses = array();
+
+    /**
+     * login login action
+     * 
+     * @access public
+     * @return void
+     */
+    function login() {
+        $this->set('login_url', $this->Auth->getLoginUrl());
+        $this->set('is_logged_in', $this->Auth->isLoggedIn());
+
+        // check if the auth module needs a login form or just a button
+        $this->set('auth_module_name', $this->Auth->getAuthModuleName());
+    }
+
+    /**
+     * logout logout action
+     * 
+     * @access public
+     * @return void
+     */
+    function logout() {
+        $this->redirect($this->Auth->logout());
+    }
+}
