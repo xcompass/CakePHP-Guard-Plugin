9e9204d74192577c28946ad36ac47d9e7253b3af
Compass <pan.luo@ubc.ca>
2012-05-23 13:57:32 -0700

    added ldap module

--- config/guard.php
+++ config/guard.php
@@ -1,17 +1,31 @@
 <?php
 
-$config['Guard.AuthModule.Name'] = 'Shibboleth';    // Using Shibboleth module
-//$config['Guard.AuthModule.Name'] = 'Default';     // Using default (build-in) module
+//$config['Guard.AuthModule.Name'] = 'Ldap';    // Using LDAP module
+//$config['Guard.AuthModule.Name'] = 'Shibboleth';    // Using Shibboleth module
+$config['Guard.AuthModule.Name'] = 'Default';     // Using default (build-in) module
 
-$config['Guard.AuthModule.Shibboleth'] = array('sessionInitiatorURL' => 'https://%HOST%/Shibboleth.sso/Login',
-                                               'logoutURL'           => 'https://%HOST%/Shibboleth.sso/Logout',
-                                               'fieldMapping'        => array('eppn'        => 'username',
-                                                                              'affiliation' => 'role',
-                                                                             ),
-                                               'mappingRules'        => array('eppn'        => array('/@ubc.ca/' => ''),
-                                                                              'affiliation' => array('/staff@ubc.ca/' => 'admin'),
-                                                                             ),
-                                               'loginError'          => 'You have successfully logged through Shibboleth. But you do not have access this appliction.',
-                                               'loginImageButton'    => '',
-                                               'loginTextButton'     => 'Login',
-                                              ); 
+$config['Guard.AuthModule.Shibboleth'] = array(
+    'sessionInitiatorURL' => 'https://%HOST%/Shibboleth.sso/Login',
+    'logoutURL'           => 'https://%HOST%/Shibboleth.sso/Logout',
+    'fieldMapping'        => array(
+        'eppn'        => 'username',
+        'affiliation' => 'role',
+    ),
+    'mappingRules'        => array(
+        'eppn'        => array('/@ubc.ca/' => ''),
+        'affiliation' => array('/staff@ubc.ca/' => 'admin'),
+    ),
+    'loginError'          => 'You have successfully logged through Shibboleth. But you do not have access this appliction.',
+    'loginImageButton'    => '',
+    'loginTextButton'     => 'Login',
+);
+
+$config['Guard.AuthModule.Ldap'] = array(
+    'host' => 'ldaps://ldapcons.stg.id.ubc.ca/',
+    'port' => 636,
+    'serviceUsername' => 'uid=ipeer, ou=Special Users, o=ubc.ca', // username to connect to LDAP
+    'servicePassword' => '', // password to connect to LDAP
+    'baseDn' => 'ou=Campus Login, o=ubc.ca',
+    'usernameField' => 'cwlLoginName',
+    'passwordField' => 'password',
+);
---
+++ controllers/components/ldap_module.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * LdapModule The LDAP authentication module. It authenticate against a LDAP
+ * server.
+ *
+ * @uses      AuthModule
+ * @package   Plugins.Guard
+ * @author    Compass <pan.luo@ubc.ca>
+ * @copyright 2012 CTLT
+ * @license   PHP Version 3.0 {@link http://www.php.net/license/3_0.txt}
+ */
+class LdapModule extends AuthModule
+{
+    /**
+     * name the name of the authentication module
+     *
+     * @var string
+     * @access public
+     */
+    var $name = 'Ldap';
+
+    /**
+     * hasLoginForm this module uses internal login page
+     *
+     * @var mixed
+     * @access public
+     */
+    var $hasLoginForm = true;
+
+    /**
+     * authenticate authenticate the user and generate the user session
+     *
+     * @param mixed $username
+     *
+     * @access public
+     * @return void
+     */
+    function authenticate($username = null)
+    {
+        $loggedIn = false;
+
+        $ds = ldap_connect($this->host, $this->port);
+
+        ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+        if (!(@ldap_bind($ds, $this->serviceUsername, $this->servicePassword))) {
+            $this->guard->error(sprintf('Could not connect to LDAP server: %s with port %d.', $this->host, $this->port));
+            return false;
+        }
+
+        if (!($result = ldap_search($ds, $this->baseDn, $this->usernameField.'='.$this->data[$this->guard->fields['username']]))) {
+            $this->guard->error(sprintf('Unable to perform LDAP seach with base DN %s and search %s.',
+                $this->baseDn, $this->usernameField.'='.$this->data[$this->guard->fields['username']]));
+            return false;
+        }
+
+        $info = ldap_get_entries($ds, $result);
+
+        if (0 != $info['count']) {
+            if (@ldap_bind($ds, $info[0]['dn'], $this->data[$this->guard->fields['password']])) {
+                // ldap success
+                if ($user = $this->identify($this->data[$this->guard->fields['username']])) {
+                    $this->Session->write($this->guard->sessionKey, $user);
+                    $loggedIn = true;
+                }
+            }
+        }
+
+        ldap_close($ds);
+        return $loggedIn;
+    }
+
+    /**
+     * identify find the user from database
+     *
+     * @param bool $username   username
+     * @param bool $conditions search condition
+     *
+     * @access public
+     * @return array user array
+     */
+    function identify($username = null, $conditions = null)
+    {
+        // get the model AuthComponent is configured to use
+        $model =& $this->guard->getModel(); // default is User
+        // do a query that will find a User record when given successful login data
+        $user = $model->find('first', array('conditions' => array(
+            $model->escapeField($this->guard->fields['username']) => $username)
+        ));
+
+        // return null if user invalid
+        if (!$user) {
+            return null; // this is what AuthComponent::identify would return on failure
+        }
+
+        // call original AuthComponent::identify with string for $user and false for $conditions
+        return $this->guard->identify($user[$this->guard->userModel][$model->primaryKey], null);
+    }
+
+}
