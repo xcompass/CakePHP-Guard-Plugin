329960c13761bdfbd591251871aea5fb1c5bc540
Compass <pan.luo@ubc.ca>
2014-02-26 15:29:46 -0800

    Change return of getLoginUrl to relative URL

--- controllers/components/auth_module.php
+++ controllers/components/auth_module.php
@@ -189,7 +189,7 @@ class AuthModule extends Object {
      * @return string the login URL
      */
     function getLoginUrl() {
-        return Router::url(array('plugin' => 'guard', 'controller' => 'guard', 'action' => 'login'), true);
+        return Router::url(array('plugin' => 'guard', 'controller' => 'guard', 'action' => 'login'), false);
     }
 
     /**
